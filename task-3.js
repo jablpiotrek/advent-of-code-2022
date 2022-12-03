831fce0c3ba08629a91721ec0f059e0cff5e5af2
Piotr Jablonski <jabl.piotrek@gmail.com>
2022-12-03 08:59:05 +0100

    task 3

---
+++ task-3.js
@@ -0,0 +1,26 @@
+fs = require('fs');
+
+const rucksacks = fs.readFileSync('input.txt', 'utf8').toString().split('\n')
+
+
+console.log(`Part 1: ${rucksacks.reduce((result, rucksack) => {
+    const commonItem = rucksack.slice(0, (rucksack.length / 2))
+        .split('').find(item => rucksack.slice((rucksack.length / 2), rucksack.length).includes(item))
+    
+    return result + commonItem.charCodeAt(0) - (commonItem.toLowerCase() === commonItem ? 96 : 38)
+}, 0)}`);
+
+console.log(`Part 2: ${rucksacks.reduce((groups, rucksack, i) => {
+    const groupIndex = Math.floor(i / 3);
+
+    if (!groups[groupIndex]) {
+        groups[groupIndex] = []
+    }
+    groups[groupIndex].push(rucksack)
+
+    return groups;
+}, []).reduce((result, group) => {
+    const commonItem = group[0].split('').find(item => (group[1].includes(item) && group[2].includes(item)))
+    
+    return result + commonItem.charCodeAt(0) - (commonItem.toLowerCase() === commonItem ? 96 : 38)
+}, 0)}`);
